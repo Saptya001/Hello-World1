2512cd2f86d339655c88404ddc01ce6c4be53a6c
SAPTYADEEP SOM <saptyadeepsomlaptop1@gmail.com>
2025-09-01 15:30:10 +0530

    Create multiply.py

---
+++ multiply.py
@@ -0,0 +1,29 @@
+# matrix_addition.py
+
+def add_matrices(matrix1, matrix2):
+   
+    if len(matrix1) != len(matrix2) or len(matrix1[0]) != len(matrix2[0]):
+        return "Error: Matrices must have the same dimensions for addition."
+    
+    result = []
+    for i in range(len(matrix1)):
+        row = []
+        for j in range(len(matrix1[0])):
+            row.append(matrix1[i][j] + matrix2[i][j])
+        result.append(row)
+    return result
+
+
+matrix1 = [
+    [1, 2, 3],
+    [4, 5, 6]
+]
+
+matrix2 = [
+    [7, 8, 9],
+    [10, 11, 12]
+]
+
+print("Matrix Addition Result:")
+for row in add_matrices(matrix1, matrix2):
+    print(row)
